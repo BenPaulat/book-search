c15a0dfea6395d8c5051b9192c31dfb8b6d7f2f9
BenPaulat <benjamin.d.paulat@gmail.com>
2022-01-23 16:33:26 -0600

    implemented Schema

---
+++ server/schema/resolvers.js
@@ -0,0 +1,39 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({
+                    _id: context.user._id
+                })
+                    .select('-__v -password')
+                return userData;
+            }
+            throw new AuthenticationError('You are not logged in');
+        },
+        // get all users
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
+        // get a user by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
+        },
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+
+        }
+    }
+}
+
+module.exports = resolvers;
